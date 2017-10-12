f8dabf5403a2d4991f3d863265023a6b85f7d843
Luke Bosse <mrlukebosse@gmail.com>
2017-10-12 02:17:02 -0600

    added mlab uri to config/database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/ded', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_pkd90lks:o8dtt8dh5ikmij373f861412op@ds117625.mlab.com:17625/heroku_pkd90lks', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
